530f75412ea86233e48c8bf3c87d7714b0639378
MuzaffarAli-Sayyed <470sayyedmuzaffarali@gmail.com>
2024-03-08 18:34:37 +0530

    update

---
+++ src/components/Footer.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <Footer>
+            <div>
+                <h1>TechyMusa</h1>
+                <p>@all right reserved</p>
+            </div>
+
+            <div>
+                <h5>Follow Us</h5>
+                <div>
+                    <a href="https://youtube.com/" target={"blank"}>
+                        Youtube
+                    </a>
+                    <a href="https://www.instagram.com/_muzaffar_ali_sayyed_/" target={"blank"}>
+                        Instagram
+                    </a>
+                    <a href="https://github.com/MuzaffarAli-Sayyed" target={"blank"}>
+                        Github
+                    </a>
+                </div>
+            </div>
+        </Footer>
+    );
+
+
+};
+
+export default Footer;
