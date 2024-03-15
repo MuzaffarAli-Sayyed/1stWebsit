3dfe41ed6453daf2a25ac47fd76fdc72ced3036b
MuzaffarAli-Sayyed <470sayyedmuzaffarali@gmail.com>
2024-03-15 14:19:38 +0530

    update

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
-import Services from "./components/Services";
+// import Services from "./components/Services";
 import Contact from "./components/Contact";
 
 import "./styles/App.scss";
 import "./styles/header.scss";
 import "./styles/home.scss";
 import "./styles/footer.scss";
-import "./styles/contact.scss";
-import "./styles/mediaquery.scss";
+
 
 function App() {
   return (
@@ -19,11 +18,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/services" element={<Services />} />
+        <Route path="/Services" element={<Services />} />
       </Routes>
       <Footer />
     </Router>
   );
 }
-
 export default App;
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,33 +3,29 @@ import React from "react";
 const Contact = () => {
   return (
     <div className="contact">
-      <main>
-        <h1>Contact Us</h1>
-
-        <form>
-          <div>
+   <main>
+    <h1>Contact Us</h1>
+    
+   
+       <form>
+        <div>
             <label>Name</label>
-            <input type="text" required placeholder="Abc" />
-          </div>
-
-          <div>
+            <input type="text" required placeholder="ABC" />
+        </div>
+        <div>
             <label>Email</label>
-            <input type="email" required placeholder="Abc@xyz.com" />
-          </div>
-          <div>
+            <input type="email" required placeholder="ABC@gmail.com" />
+        </div>
+        <div>
             <label>Message</label>
-            <input
-              type="text"
-              required
-              placeholder="Tell us about your query..."
-            />
-          </div>
+            <input type="text" required placeholder="Tell us about your query.." />
+        </div>
+        <button type="sumit ">Send</button>
+        </form>   
 
-          <button type="submit">Send</button>
-        </form>
-      </main>
+    </main>
     </div>
-  );
+  )
 };
 
 export default Contact;
